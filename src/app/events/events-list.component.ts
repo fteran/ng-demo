01d1468d195ec83914ead2cefdfdb97f3c6f9026
fteran <fteran@contactsolutions>
2018-09-11 09:06:37 -0500

    Communicating with Child Component

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -2,18 +2,20 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'events-list',
-  template: `
-    <div>
-      <h1>Upcoming Angular Events</h1>
-      <hr>
-      <h2>{{event.name}}</h2>
-    </div>
-  `
+  templateUrl: './events-list.component.html'
 })
 export class EventsListComponent {
-  event = {
+  event1 = {
     id: 1,
-    name: 'Angular Connect'
+    name: 'Angular Connect',
+    date: '9/26/2018',
+    time: '10:00 AM',
+    price: 599.99,
+    imageUrl: '/assets/images/angularconnect-shild.png',
+    location: {
+      address: '1057 DT',
+      city: 'Dallas',
+      country: "United States"
+    }
   }
-
 }
